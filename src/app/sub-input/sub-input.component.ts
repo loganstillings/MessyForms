e1af08c75042003a61be1c324fb6cdde3e7a9018
Logan Stillings <lstillin@u.rochester.edu>
2019-06-16 18:00:53 -0400

    Finish conditional form logic

--- src/app/sub-input/sub-input.component.ts
+++ src/app/sub-input/sub-input.component.ts
@@ -1,37 +1,51 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { IConditionType } from 'src/model/interfaces/condition-type';
 import { ConditionTypes } from 'src/model/static-lists/condition-types';
 import { QuestionTypesEnum } from 'src/model/enums/QuestionTypes';
 import { ConditionTypesEnum } from 'src/model/enums/ConditionTypes';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-sub-input',
   templateUrl: './sub-input.component.html',
 })
-export class SubInputComponent implements OnInit {
+export class SubInputComponent implements OnInit, OnDestroy {
   @Input('index') index: number;
   @Input('formGroup') formGroup: FormGroup;
   conditionTypes: IConditionType[] = [];
   parentQuestionTypeId: number;
+  questionTypesEnum = QuestionTypesEnum;
+  subscriptions: Subscription = new Subscription();
   constructor() {}
 
   ngOnInit() {
     this.getParentQuestionTypeId();
+    this.subscribeToParentQuestionTypeIdChange();
   }
 
   getParentQuestionTypeId() {
-    console.log(this.formGroup);
     this.parentQuestionTypeId = this.formGroup.parent.parent.get(
       'QuestionTypeId',
     ).value;
-    console.log(this.parentQuestionTypeId);
     if (this.parentQuestionTypeId != QuestionTypesEnum.Number) {
       this.conditionTypes = ConditionTypes.filter((ct) => {
-        ct.Id == ConditionTypesEnum.Equals;
+        return ct.Id == ConditionTypesEnum.Equals;
       });
     } else {
       this.conditionTypes = ConditionTypes;
     }
+  }
+
+  subscribeToParentQuestionTypeIdChange(): void {
+    this.subscriptions = this.formGroup.parent.parent
+      .get('QuestionTypeId')
+      .valueChanges.subscribe((change) => {
+        this.getParentQuestionTypeId();
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 }
